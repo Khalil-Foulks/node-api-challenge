7558e871123f2cf18b621ea521dcb4e664565d8c
Khalil <khalilfoulks@gmail.com>
2020-08-07 11:30:18 -0400

    setup router structure

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -3,4 +3,25 @@ const Actionsdb = require('../data/helpers/actionModel')
 
 const router = express.Router();
 
+
+// Retrieve ALL actions
+router.get('/', (req, res) => {
+
+})
+
+// Retrieve an action by action id
+router.get('/:id', (req, res) => {
+
+})
+
+//REMOVE an existing action
+router.delete('/:id', (req, res) => {
+
+})
+
+//UPDATE an existing action
+router.put('/:id', (req, res) => {
+
+})
+
 module.exports = router;
--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -6,4 +6,39 @@ const customMw = require('../customMiddleware/customMiddleware')
 
 const router = express.Router();
 
+// Retrieve ALL projects
+router.get('/', (req, res) => {
+
+})
+
+// Retrieve ALL projects by project id
+router.get('/:id', (req, res) => {
+
+})
+
+// Retrieve ALL actions by project id
+router.get('/:id/actions', (req, res) =>{
+
+})
+
+// ADD a project to list of projects
+router.post('/', (req, res) => {
+
+})
+
+// ADD an action to a project by project id
+router.post('/:id/actions', customMw.validateProjectId, (req, res) => {
+
+})
+
+//REMOVE an existing project
+router.delete('/:id', (req, res) => {
+
+})
+
+//UPDATE an existing project
+router.put('/:id', (req, res) => {
+
+})
+
 module.exports = router;
