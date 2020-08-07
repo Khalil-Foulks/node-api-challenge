f827bfee4e5aaa727580540776563939b65293f6
Khalil <khalilfoulks@gmail.com>
2020-08-07 12:09:18 -0400

    finished creating endpoints

--- customMiddleware/customMiddleware.js
+++ customMiddleware/customMiddleware.js
@@ -6,5 +6,18 @@ module.exports = {
 }
 
 function validateProjectId(req, res, next) {
-
+    const id = req.params.id
+    
+    Projectsdb.get(id)
+    .then(project => {
+        if(project){
+            next();
+        } else {
+            res.status(400).json({ message: "invalid project id" })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    })
 }
